d65546b6def00c20284f6965d8e60a099f5c43bf
MaxKomar <maxkomar65@gmail.com>
2016-04-14 19:05:20 +0300

    add event listener to mouseDown

--- app/directives/slider/slider.ts
+++ app/directives/slider/slider.ts
@@ -1,4 +1,4 @@
-import {Component, Input} from 'angular2/core';
+import {Component, Input, HostListener, ElementRef} from 'angular2/core';
 
 @Component ({
     selector: 'slider',
@@ -6,6 +6,18 @@ import {Component, Input} from 'angular2/core';
 })
 
 export default class Slider {
+    width:number;
     @Input() buffered: number;
     @Input() amount: number;
+
+    constructor(element: ElementRef){
+        this.width = element.nativeElement.clientWidth;
+        console.log(this.width);
+    }
+
+    @HostListener("mousedown", ["$event.offsetX"])
+    onMoseDown(offset):void{
+        this.amount = offset/this.width*100;
+        console.log(offset);
+    }
 }
